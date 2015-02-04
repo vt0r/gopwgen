d987d1d079732c145f58206797ea880ca26f38fe
Salvatore LaMendola <salvatore.lamendola@gmail.com>
2015-02-04 09:49:31 -0500

    Complete rewrite. Separate password generator function to eliminate code duplication, crypto/rand instead of math/rand, proper flag usage, etc.

--- gopwgen.go
+++ gopwgen.go
@@ -1,171 +1,104 @@
-// Sal's Random Password Generator
-// --------------------------------
-// My very first Go application.
-// Usage is printed by running 'gopwgen h'
-// --------------------------------
-// Copyright 2013 by Salvatore LaMendola
-// Permission is granted to copy/modify
-// in any way you please as long as proper
-// credit is given. Please see LICENSE for
-// more details (GPLv3).
+/*
+ * Sal's Random Password Generator
+ * --------------------------------
+ * My very first Go application.
+ * Usage is printed by running 'gopwgen h'
+ * --------------------------------
+ * Copyright 2013 by Salvatore LaMendola
+ * Permission is granted to copy/modify
+ * in any way you please as long as proper
+ * credit is given. Please see LICENSE for
+ * more details (GPLv3).
+ */
 
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "time"
-    "os"
-    "flag"
-    "strconv"
+	"crypto/rand"
+	"flag"
+	"fmt"
+	"io"
 )
 
-// Function to print usage information
-func usage() {
-    fmt.Println("Sal's Random Password Generator")
-    fmt.Println("-------------------------------")
-    fmt.Println("Usage: gopwgen <OPTION> [length] [number] (length and number optional)\n")
-    fmt.Println("OPTIONS (MUST SPECIFY ONE!):")
-    fmt.Println("s | symbols      Alphanumeric + symbols (NOT FOR MYSQL!)")
-    fmt.Println("a | alpha        Alphanumeric only")
-    fmt.Println("p | phpmyadmin   Generate phpMyAdmin Blowfish secret (for cookie auth)")
-    fmt.Println("w | wordpress    Generate Wordpress encryption keys (wp-config.php)")
-    fmt.Println("h | help         Display this usage information\n")
-    fmt.Println("If no length or number are defined (and you haven't changed the code), a default length of 19 and number of 1 will be used.")
-    os.Exit(0)
-}
+/*
+ * Proper flag variable assignment and usage info
+ * Thanks to Jacques Fuentes for the code review!
+ */
+var (
+	flagSymbols    = flag.Bool("s", false, "Alphanumeric + symbols (NOT FOR MYSQL!)")
+	flagAlpha      = flag.Bool("a", false, "Alphanumeric only")
+	flagPhpMyAdmin = flag.Bool("p", false, "Generate phpMyAdmin Blowfish secret (for cookie auth)")
+	flagWordPress  = flag.Bool("w", false, "Generate WordPress encryption salts for use in wp-config.php")
+	flagLength     = flag.Int("l", 19, "Length of generated password(s)")
+	flagNumber     = flag.Int("n", 1, "Number of generated password(s)")
+)
+
+// Alphanumeric values and symbols+alpha
+const alphanumeric = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789"
+const symbols = alphanumeric + "-_!@#$%^&*/\\()_+{}|:<>?="
 
 func main() {
-    // Alphanumeric values
-    alphanumeric := "abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789"
+	flag.Parse()
 
-    // Symbols + alpha values
-    symbols := "abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789-_!@#$%^&*/\\()_+{}|:<>?="
+	// Generate alphanumeric password(s)
+	if *flagAlpha {
+		for i := 0; i < *flagNumber; i++ {
+			password := pwgen(*flagLength, false)
+			fmt.Println(string(password))
+		}
+	}
 
-    // Set default length+number if no args, or
-    // just read the args and populate one/both
-    var length int
-    var number int
+	// Generate alpha/symbols password(s)
+	if *flagSymbols {
+		for i := 0; i < *flagNumber; i++ {
+			password := pwgen(*flagLength, true)
+			fmt.Println(string(password))
+		}
+	}
 
-    flag.Parse()
-    if flag.Arg(1) == "" && flag.Arg(2) == "" {
-        length = 19
-        number = 1
-    } else if flag.Arg(2) == "" {
-        s1 := flag.Arg(1)
-        value1, err1 := strconv.Atoi(s1)
-        if err1 != nil {
-            fmt.Println(err1)
-            os.Exit(2)
-        }
-        length = value1
-        number = 1
-    } else {
-        s1 := flag.Arg(1)
-        s2 := flag.Arg(2)
-        value1, err1 := strconv.Atoi(s1)
-        if err1 != nil {
-            fmt.Println(err1)
-            os.Exit(2)
-        }
-        value2, err2 := strconv.Atoi(s2)
-        if err2 != nil {
-            fmt.Println(err2)
-            os.Exit(2)
-        }
-        length = value1
-        number = value2
-    }
+	// Generate phpMyAdmin blowfish secret
+	if *flagPhpMyAdmin {
+		password := pwgen(64, true)
+		fmt.Println(string(password))
+	}
 
-    // Seed the RNG
-    rand.Seed(time.Now().UnixNano())
+	// Generate WordPress encryption secrets
+	if *flagWordPress {
+		password1 := pwgen(64, true)
+		password2 := pwgen(64, true)
+		password3 := pwgen(64, true)
+		password4 := pwgen(64, true)
+		password5 := pwgen(64, true)
+		password6 := pwgen(64, true)
+		password7 := pwgen(64, true)
+		password8 := pwgen(64, true)
 
-    // Create the password string
-    password := make([]byte, length)
+		fmt.Printf("define('AUTH_KEY',\t\t'%s');\n", password1)
+		fmt.Printf("define('SECURE_AUTH_KEY',\t'%s');\n", password2)
+		fmt.Printf("define('LOGGED_IN_KEY',\t\t'%s');\n", password3)
+		fmt.Printf("define('NONCE_KEY',\t\t'%s');\n", password4)
+		fmt.Printf("define('AUTH_SALT',\t\t'%s');\n", password5)
+		fmt.Printf("define('SECURE_AUTH_SALT',\t'%s');\n", password6)
+		fmt.Printf("define('LOGGED_IN_SALT',\t'%s');\n", password7)
+		fmt.Printf("define('NONCE_SALT',\t\t'%s');\n", password8)
 
-    // Generate alphanumeric password(s)
-    if flag.Arg(0) == "a" || flag.Arg(0) == "alpha" {
-        for i := 0; i < number; i++ {
-            for j := 0; j < length; j++ {
-                password[j] = alphanumeric[rand.Intn(len(alphanumeric))]
-            }
+	}
 
-            fmt.Println(string(password))
-        }
-    }
+}
 
-    // Generate alphanumeric + symbols password(s)
-    if flag.Arg(0) == "s" || flag.Arg(0) == "symbols" {
-        for i := 0; i < number; i++ {
-            for j := 0; j < length; j++ {
-                password[j] = symbols[rand.Intn(len(symbols))]
-            }
+func pwgen(length int, symbolsenabled bool) []byte {
+	// Create the password string and associated randomness
+	password := make([]byte, length)
+	entropy := make([]byte, length+(length/4))
 
-            fmt.Println(string(password))
-        }
-    }
-
-    // Generate phpMyAdmin blowfish secret
-    if flag.Arg(0) == "p" || flag.Arg(0) == "phpmyadmin" {
-        password := make([]byte, 64)
-        for j := 0; j < 64; j++ {
-            password[j] = symbols[rand.Intn(len(symbols))]
-        }
-
-        fmt.Println(string(password))
-
-    }
-
-    // Generate WordPress encryption secrets
-    if flag.Arg(0) == "w" || flag.Arg(0) == "wordpress" {
-        password1 := make([]byte, 64)
-        password2 := make([]byte, 64)
-        password3 := make([]byte, 64)
-        password4 := make([]byte, 64)
-        password5 := make([]byte, 64)
-        password6 := make([]byte, 64)
-        password7 := make([]byte, 64)
-        password8 := make([]byte, 64)
-
-        for j := 0; j < 64; j++ {
-            password1[j] = symbols[rand.Intn(len(symbols))]
-        }
-        for j := 0; j < 64; j++ {
-            password2[j] = symbols[rand.Intn(len(symbols))]
-        }
-        for j := 0; j < 64; j++ {
-            password3[j] = symbols[rand.Intn(len(symbols))]
-        }
-        for j := 0; j < 64; j++ {
-            password4[j] = symbols[rand.Intn(len(symbols))]
-        }
-        for j := 0; j < 64; j++ {
-            password5[j] = symbols[rand.Intn(len(symbols))]
-        }
-        for j := 0; j < 64; j++ {
-            password6[j] = symbols[rand.Intn(len(symbols))]
-        }
-        for j := 0; j < 64; j++ {
-            password7[j] = symbols[rand.Intn(len(symbols))]
-        }
-        for j := 0; j < 64; j++ {
-            password8[j] = symbols[rand.Intn(len(symbols))]
-        }
-
-        fmt.Printf("define('AUTH_KEY',\t\t'%s');\n",password1)
-        fmt.Printf("define('SECURE_AUTH_KEY',\t'%s');\n",password2)
-        fmt.Printf("define('LOGGED_IN_KEY',\t\t'%s');\n",password3)
-        fmt.Printf("define('NONCE_KEY',\t\t'%s');\n",password4)
-        fmt.Printf("define('AUTH_SALT',\t\t'%s');\n",password5)
-        fmt.Printf("define('SECURE_AUTH_SALT',\t'%s');\n",password6)
-        fmt.Printf("define('LOGGED_IN_SALT',\t'%s');\n",password7)
-        fmt.Printf("define('NONCE_SALT',\t\t'%s');\n",password8)
-
-    }
-
-    // Print usage info
-    if flag.Arg(0) == "h" || flag.Arg(0) == "help" || flag.Arg(0) == "" {
-        usage()
-    }
-
+	// Generate password(s) of the requested length/count
+	io.ReadFull(rand.Reader, entropy)
+	for j := 0; j < length; j++ {
+		if symbolsenabled {
+			password[j] = symbols[entropy[j]%byte(len(symbols))]
+		} else {
+			password[j] = alphanumeric[entropy[j]%byte(len(alphanumeric))]
+		}
+	}
+	return password
 }
