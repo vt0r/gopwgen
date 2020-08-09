c7220cda77af1bc7c78902f74aee92d4376d9178
Salvatore LaMendola <salvatore@lamendola.me>
2020-08-09 00:54:37 -0400

    Version 2.2.0 migrates to proper go modules for dependencies

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/vt0r/gopwgen
+
+go 1.14
+
+require github.com/sethvargo/go-diceware v0.2.0
--- gopwgen.go
+++ gopwgen.go
@@ -54,7 +54,7 @@ const hexadecimal = "abcdef0123456789"
 const defaultlen = 19
 const defaultnum = 1
 const defaultDwLen = 6
-const version = "2.1.0"
+const version = "2.2.0"
 
 func myUsage() {
 	fmt.Printf("Usage: %s [OPTION] [length | num dice words] [num pwds/phrases]\n\nOptions:\n", os.Args[0])
